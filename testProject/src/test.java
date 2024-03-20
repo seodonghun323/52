fe2b66d6d30841b1a0efda00826461e3cdaf5e1f
seodonghun323 <163955605+seodonghun323@users.noreply.github.com>
2024-03-20 10:04:41 +0900

    first commit

---
+++ testProject/src/test.java
@@ -0,0 +1,12 @@
+public class test {
+
+    public static void main(String[] args) {
+
+        System.out.println("단비는 바보");
+        System.out.println("QNQNNQN");
+
+    System.out.print("ㅜ쀼쀼");
+        System.out.print("삐삐삐");
+
+    }
+}
