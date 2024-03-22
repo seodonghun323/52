ddd23a93c0e760b1ae6a4b7832202bef316f86e4
seodonghun323 <163955605+seodonghun323@users.noreply.github.com>
2024-03-22 17:30:38 +0900

    no message

---
+++ 01-lecture-source/chap03-method-and-api-lecture-source/src/com/ohgiraffers/section01/method/Calculator.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.section01.method;
+
+public class Calculator {
+
+    public int minNumberOF(int first,int second){
+
+        return(first>second)? second : first;
+    }
+
+    public static int maxNumberOf(int first, int second){
+
+        return (first>second)? first : second;
+    }
+}
---
+++ 01-lecture-source/chap03-method-and-api-lecture-source/src/com/ohgiraffers/section02/packge_and_import/Application3.java
@@ -0,0 +1,22 @@
+package com.ohgiraffers.section02.packge_and_import;
+
+import com.ohgiraffers.section01.method.Application5;
+import com.ohgiraffers.section01.method.Calculator;
+
+import java.sql.SQLOutput;
+
+public class Application3 {
+    public static void main(String[] args) {
+
+
+//        System.out.println(oh.minNumberOF(20,30));
+        System.out.println(Calculator.maxNumberOf(20,30));
+
+
+
+
+
+
+
+    }
+}
